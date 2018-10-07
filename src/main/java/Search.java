b87657980cc43588a3ea7905bf0ea160aabaf3aa
Yura <sevasjin@ya.ru>
2018-10-07 20:58:40 +0300

    Подсчёт для одной страницы, без логера

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,8 +1,15 @@
+import org.javatuples.Pair;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-
-import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class Search {
 
@@ -23,6 +30,11 @@ public class Search {
     @FindBy(css="td[dir=\"ltr\"] > select[class=\"ui-pg-selbox\"] > option[value=\"100\"]")
     private WebElement selectListSize = null;
 
+    @FindAll({
+            @FindBy(css="tbody > tr[role=\"row\"][tabindex=\"-1\"]")
+    })
+    private List<WebElement> rowInList = null;
+
     Search(WebDriver driver){
         PageFactory.initElements(driver, this);
         this.driver = driver;
@@ -34,5 +46,35 @@ public class Search {
     void FillCheck223(){ checkNorm223.click(); }
     void FillCheckCommercialPurchase(){ checkCommercialPurchase.click(); }
     void FillStartCost(String str){ startCost.sendKeys(str); }
-    void setListSize(){selectListSize.click();}
+    void setListSize(){
+        (new WebDriverWait(driver,10)).until(
+                ExpectedConditions.invisibilityOfElementLocated(By
+                        .cssSelector("#load_BaseMainContent_MainContent_jqgTrade"))
+        );
+        selectListSize.click();
+    }
+    Pair<Double, Double> focusRow(){
+        double ids=0;
+        double allsum=0;
+        (new WebDriverWait(driver,20)).until(new ExpectedCondition<Boolean>(){
+            public Boolean apply(WebDriver d) {
+                return rowInList.size() == 100;
+            }
+        });
+        for (WebElement row: rowInList) {
+            if (!(row.findElement(By
+                    .cssSelector("td[aria-describedby=\"BaseMainContent_MainContent_jqgTrade_OosNumber\"]"))
+                    .getText()
+                    .equals("&nbsp;"))
+                    //!= "&nbsp;"
+            ){
+                ids++;
+                allsum += Double.parseDouble( row.findElement(By
+                        .cssSelector("td[aria-describedby=\"BaseMainContent_MainContent_jqgTrade_StartPrice\"]"))
+                        .getText().replaceAll("[^\\.0123456789]","")
+                );
+            }
+        }
+        return Pair.with(ids , allsum);
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,8 @@
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
 
 public class MainTest {
 
@@ -15,7 +17,7 @@ public class MainTest {
     public void close(){
         Driver.finish();
     }
-
+/*
     @Test
     public void fillStartCost(){
         s.FillStartCost("0");
@@ -34,5 +36,20 @@ public class MainTest {
     public void set_ListSize(){
         //$("td[dir=\"ltr\"] > select[class=\"ui-pg-selbox\"] > option[value=\"100\"]").click();
         s.setListSize();
+    }
+
+    @Test
+    public void checkRow(){
+        System.out.println(s.focusRow()); //s.focusRow();
+    }
+    */
+    @Test
+    public void MainTest(){
+        s.FillStartCost("0");
+        s.FillCheck223();
+        s.FillCheckCommercialPurchase();
+        s.SelectSearch();
+        s.setListSize();
+        System.out.println(s.focusRow());
     }
 }
